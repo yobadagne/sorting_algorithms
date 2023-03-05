4779cac7b03a71ed5fdf953cd178bfd8d8893087
yobadagne <yobadagne@gmail.com>
2023-03-05 07:55:52 +0000

    0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+/**
+  * bubble_sort - function that sorts an array of integers
+  * in ascending order using the Bubble sort algorithm
+  * @array: is data
+  * @size: is size data
+  * Return: nothing
+  */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i;
+	size_t j;
+	int tmp;
+
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}
